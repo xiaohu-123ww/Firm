d74ce569304a409a982ec36230439fba4394f606
huh <15581952631@163.com>
2022-10-21 11:32:33 +0800

    登录框架

---
+++ src/views/Login/components/login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="login-content">
+      <el-row>
+        <el-col :span="13"
+          ><div class="login-image">
+            <div class="image">
+              <img
+                src="../../.../../../assets/picture/beijingkejigan@2x.png"
+                alt=""
+              />
+            </div></div
+        ></el-col>
+        <el-col :span="11"
+          ><div class="login-form">
+            <div class="form"></div></div
+        ></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped lang="scss">
+.login-content {
+  width: 100%;
+  height: 600px;
+  background-color: #f5f5f5;
+  padding: 40px;
+  .login-image {
+    width: 100%;
+    height: 525px;
+    // background-color: pink;
+    padding-top: 55px;
+    .image {
+      width: 578px;
+      height: 406px;
+      // background-color: aqua;
+      margin: 0 auto;
+      border: 1px dashed #999;
+    }
+  }
+  .login-form {
+    width: 100%;
+    height: 550px;
+    // background-color: red;
+    .form {
+      width: 520px;
+      height: 525px;
+      background-color: #fff;
+      border-radius: 20px;
+    }
+  }
+}
+.login-footer {
+  width: 100%;
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,59 @@
 <template>
   <div class="login-container">
-    <div class="login-form"></div>
+    <Login />
+    <div class="login-from">
+      <el-row>
+        <el-col :span="5">
+          <div
+            class="login-span"
+            style="margin-bottom: 20px; color: #999; font-size: 13px"
+          >
+            企业服务
+          </div>
+          <div class="login-span">智能制造技能培训</div>
+          <div class="login-span">产业合作</div>
+          <div class="login-span">人力资源服务</div>
+        </el-col>
+        <el-col :span="9">
+          <div
+            class="login-span"
+            style="margin-bottom: 20px; color: #999; font-size: 13px"
+          >
+            联系我们
+          </div>
+          <div class="login-span">企业服务热线 186-1021-8901</div>
+          <div class="login-span">企业邮箱 talent@shiyenet.com.cn</div>
+          <div class="login-span">工作时间 10：00-18：00</div>
+        </el-col>
+        <el-col :span="8">
+          <div class="login-firm">
+            <img src="../../assets/picture/logo.png" alt="" />
+          </div>
+          <div class="image">
+            <div class="login-znzz">
+              <img src="../../assets/picture/douyin@2x.png" alt="" />
+              <div style="font-size: 13px">抖音</div>
+            </div>
+            <div class="login-znzz">
+              <img src="../../assets/picture/xiaochengxu@2x.png" alt="" />
+              <div style="font-size: 13px">小程序</div>
+            </div>
+            <div class="login-znzz">
+              <img src="../../assets/picture/dingyuehao@2x.png" alt="" />
+              <div style="font-size: 13px">订阅号</div>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <Footer class="footer" />
   </div>
 </template>
 <script>
+import Login from './components/login.vue'
+import Footer from '@/views/footer/index.vue'
 export default {
+  components: { Login, Footer },
   data () {
     return {
 
@@ -23,15 +72,50 @@ export default {
 </script>
 <style scoped lang="scss">
 .login-container {
-  background-image: url('../../assets/image/logoback.png');
+  // background-image: url('../../assets/image/logoback.png');
   width: 100%;
   height: 100vh;
-  padding-top: 200px;
-  .login-form {
-    width: 400px;
-    height: 500px;
+  background-color: #fff;
+
+  position: relative;
+  .login-from {
+    padding: 30px 160px;
+    .login-span {
+      height: 35px;
+      // background-color: pink;
+      font-size: 14px;
+    }
+    .login-firm {
+      width: 100%;
+      height: 50px;
+      // background-color: rgb(194, 192, 255);
+    }
+    .image {
+      width: 100%;
+      height: 120px;
+      // background-color: blueviolet;
+      display: flex;
+      .login-znzz {
+        width: 95px;
+        height: 120px;
+        margin-top: 10px;
+        // background-color: brown;
+        text-align: center;
+        margin-right: 43px;
+        color: #666;
+      }
+    }
+  }
+  .footer {
+    height: 30px;
     background-color: #fff;
-    margin: 0 auto;
+    text-align: center;
+    color: #a4a6a8;
+    font-size: 15px;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    bottom: 0;
   }
 }
 </style>
