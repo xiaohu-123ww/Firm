a032b76da5f935417ae1a27063fe37e958c1c97c
huh <15581952631@163.com>
2022-11-19 15:33:58 +0800

    修改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -120,13 +120,13 @@ export default {
         // 重新登录的时候做一个判断 如果发生路径上有这个参数 就以这个参数为主 如果没有这个参数
         // 还跳转到首页
         this.$router.push({
-          path: '/',
-          query: {
-            // 携带的路由参数
-            redirect: this.$route.fullPath
-            // to.path -> 只包含路径
-            // fullPatch 既有路径也有参数
-          }
+          path: '/'
+          // query: {
+          //   // 携带的路由参数
+          //   redirect: this.$route.fullPath
+          //   // to.path -> 只包含路径
+          //   // fullPatch 既有路径也有参数
+          // }
         })
       }).catch(() => {
 
